0efce1fd123bf15ab78218955e6ff2105093b3cc
lishiqian <lishiqian@changingedu.com>
2018-01-12 10:05:34 +0800

    2018-01-12 新增复杂链表的复制

---
+++ src/main/java/offer/N025_ComplexLinkedClone.java
@@ -0,0 +1,83 @@
+package offer;
+
+import common.RandomListNode;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class N025_ComplexLinkedClone {
+
+    //复制拆分法
+//    public RandomListNode Clone(RandomListNode pHead) {
+//        if(pHead == null) return null;
+//        RandomListNode cur = pHead;
+//
+//        //复制当前结点插入当前节点后面
+//        while (cur != null){
+//            RandomListNode copyNode = new RandomListNode(cur.label);
+//            copyNode.next = cur.next;
+//            cur.next = copyNode;
+//            cur = cur.next.next;
+//        }
+//
+//        //随机指针的复制
+//        cur = pHead;
+//        while (cur != null){
+//            if(cur.random != null) {
+//                cur.next.random = cur.random.next;
+//            }
+//            cur = cur.next.next;
+//        }
+//
+//        //拆分链表
+//        RandomListNode copyHead = pHead.next;
+//        RandomListNode copyCur = copyHead;
+//        cur = pHead;
+//        while (cur != null){
+//            cur.next = cur.next.next;
+//            if(copyCur.next != null){
+//                copyCur.next = copyCur.next.next;
+//            }else{
+//                copyCur.next = null;
+//            }
+//            cur = cur.next;
+//            copyCur = copyCur.next;
+//        }
+//
+//        return copyHead;
+//    }
+
+    //hash表法
+    public RandomListNode Clone(RandomListNode pHead) {
+        if(pHead == null) return null;
+        // k 原节点 v 复制节点
+        HashMap<RandomListNode,RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
+        RandomListNode cur = pHead;
+        RandomListNode copyHead = null;
+        RandomListNode curCopy = null;
+
+        while (cur != null){
+            if(copyHead == null){
+                curCopy = new RandomListNode(cur.label);
+                copyHead = curCopy;
+            }else{
+                curCopy.next = new RandomListNode(cur.label);
+                curCopy = curCopy.next;
+            }
+            map.put(cur,curCopy);
+            cur = cur.next;
+        }
+
+
+        cur = pHead;
+        curCopy = copyHead;
+        while (cur != null){
+            curCopy.random = map.get(cur.random);
+            cur = cur.next;
+            curCopy = curCopy.next;
+        }
+
+        return copyHead;
+    }
+}
